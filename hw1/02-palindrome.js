784fafed386b362c7a01ab293a60824831afa556
Max Lantas <mnxndev@outlook.com>
2023-10-03 12:59:56 -0700

    hw1: add input handler

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,22 @@
-const elem = document.querySelector('input');
+const elem = document.querySelector("input");
+const output = document.querySelector("#output");
 
-elem.addEventListener('input', handleInput);
+elem.addEventListener("input", handleInput);
+
+function handleInput(event) {
+  const original = parseFloat(elem.value);
+  let reversed = 0;
+  for (let x = original; x > 0; x = Math.floor(x / 10)) {
+    reversed *= 10;
+    reversed += x % 10;
+    console.log(x, reversed);
+  }
+
+  if (reversed === original) {
+    output.className = "text-success";
+    output.textContent = "Yes. This is a palindrome!";
+  } else {
+    output.className = "text-danger";
+    output.textContent = "No. Try again.";
+  }
+}
