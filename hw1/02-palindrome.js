b73d142e8aa0060df5a22095bee08beaa6c91e8b
Max Lantas <mnxndev@outlook.com>
2023-10-03 13:04:49 -0700

    hw1: add Math.floor integer division attribution

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -21,6 +21,13 @@ function handleInput(event) {
   }
 
   let reversed = 0;
+
+  // The Math.floor technique for integer division is from
+  // https://stackoverflow.com/a/4228376
+  //
+  // Since numbers in JavaScript are floating point, the division operator
+  // does not do integer division. The floor function removes the decimal
+  // component and allows the loop to terminate when x reaches zero.
   for (let x = original; x > 0; x = Math.floor(x / 10)) {
     reversed *= 10;
     reversed += x % 10;
