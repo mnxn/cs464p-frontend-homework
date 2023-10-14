ac16b98f2b7dcd05dc99ba12e10bc9bd1df1de0c
Max Lantas <mnxndev@outlook.com>
2023-10-14 12:39:28 -0700

    hw2: form exercise

--- hw2/05-form.js
+++ hw2/05-form.js
@@ -1,1 +1,25 @@
-// Add your code here
+const form = document.querySelector('#main-form');
+const nameInput = form.elements.name;
+const emailInput = form.elements.email;
+const statusInput = form.elements.status;
+const coursesInput = form.elements.courses;
+const commentsInput = form.elements.comments;
+
+const handleInput = function handleInput(event) {
+  event.preventDefault();
+
+  const courses = Array.from(coursesInput)
+    .filter((e) => e.checked)
+    .map((e) => e.value)
+    .join(', ');
+
+  console.group('Form Submission');
+  console.log(`Full Name: ${nameInput.value}`);
+  console.log(`Email: ${emailInput.value}`);
+  console.log(`Registration Status: ${statusInput.value}`);
+  console.log(`Courses: ${courses}`);
+  console.log(`Comments: ${commentsInput.value}`);
+  console.groupEnd();
+};
+
+form.addEventListener('submit', handleInput);
