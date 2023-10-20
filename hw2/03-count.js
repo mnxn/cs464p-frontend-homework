bb539989b22c9c6d79422960c10f4fb2d3cf711a
Max Lantas <mnxndev@outlook.com>
2023-10-20 16:31:26 -0700

    hw2: simplify highlighting function

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,42 +1,23 @@
 const textContainer = document.querySelector('#text');
 const input = document.querySelector('#word-input');
 
-const originalText = textContainer.textContent;
-
 const handleKeyDown = function handleKeyDown(event) {
-  // This event is handled before any new characters are added to the input
-  // element's value. To simplify the handling of the user input, the default
-  // behavior is prevented and the changes to the input value are handled
-  // manually.Below, the reassignment to input.value changes what the user
-  // sees on the webpage as well as the value inside this script.
-  event.preventDefault();
-
-  if (event.key.length === 1) {
-    // Checking the length of the key property of the event is a simple way to
-    // check if it is a character that is being written by the user instead of
-    // a non-character key like Control, Backspace, etc. According to the linked
-    // answer below, all inputted characters have have a length of 1 for the key
-    // property, and are therefore safe to append to the input value.
-    //
-    // Method originally found here: https://stackoverflow.com/a/49612427
-    input.value += event.key;
-  } else if (event.key === 'Backspace') {
-    // Since the default event handling is being prevented above, backspacing
-    // must be implemented manually by removing the last string character.
-    input.value = input.value.slice(0, -1);
-  }
+  // only highlight on enter
+  if (event.key !== 'Enter') return;
 
   const target = input.value;
-  if (target === '') {
-    textContainer.textContent = originalText;
-  } else {
-    // find target surrounded by word boundaries, globally (all results)
-    const regexp = new RegExp(`\\b${target}\\b`, 'g');
-    textContainer.innerHTML = originalText.replace(
-      regexp,
-      `<span class="highlight">${target}</span>`,
-    );
-  }
+
+  textContainer.innerHTML = textContainer.textContent
+    .split(/\b/) // split by word boundary
+    .map(
+      (word) => word.replace(
+        // case insensitive find, match whole string
+        new RegExp(`^${target}$`, 'i'),
+        // replace with span around original text
+        '<span class="highlight">$&</span>',
+      ),
+    )
+    .join(''); // join back to a string
 };
 
 input.addEventListener('keydown', handleKeyDown);
