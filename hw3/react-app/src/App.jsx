9c7d8b8a423bd04a53478b891a4d94e728bdd0f2
Max Lantas <mnxndev@outlook.com>
2023-10-29 22:42:31 -0700

    hw3: react home

--- hw3/react-app/src/App.jsx
+++ hw3/react-app/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+
+import Home from './Home';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar className="bg-body">
+        <Container>
+          <Navbar.Toggle />
+          <Navbar.Collapse>
+            <Nav>
+              <LinkContainer to="/home">
+                <Nav.Link>Home</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/search">
+                <Nav.Link>Search</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/houses">
+                <Nav.Link>Houses</Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <main className="container py-4 d-flex justify-content-center">
+        <Routes>
+          <Route index element={<Navigate to="/home" replace />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ hw3/react-app/src/Home.jsx
@@ -0,0 +1,11 @@
+import { Card } from 'react-bootstrap';
+
+function Home() {
+  return (
+    <>
+      <Card className="px-5 py-4 text-center d-inline-block">Welcome!</Card>
+    </>
+  );
+}
+
+export default Home;
