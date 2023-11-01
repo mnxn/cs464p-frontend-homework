6fa69f5252314650803f6e1ec84b6fe3d7fd5a2b
Max Lantas <mnxndev@outlook.com>
2023-11-01 01:16:13 -0700

    hw3: refine search on type

--- hw3/react-app/src/Search.jsx
+++ hw3/react-app/src/Search.jsx
@@ -5,54 +5,35 @@ import './Search.css';
 
 function Search({ characters }) {
   const [matchingCharacters, setMatchingCharacters] = useState([]);
-  const [searchInput, setSearchInput] = useState('');
+  const [search, setSearch] = useState('');
 
-  const filterCharacters = function filterMatchingCharacters(search) {
+  useEffect(() => {
     const lowerSearch = search.toLowerCase();
     const matching = characters.filter(
       ({ fullName, title }) => fullName.toLowerCase().includes(lowerSearch)
         || title.toLowerCase().includes(lowerSearch),
     );
     setMatchingCharacters(matching);
-  };
-
-  useEffect(() => {
-    setMatchingCharacters(characters);
-  }, [characters]);
+  }, [characters, search]);
 
   return (
     <>
       <Card className="p-4 mx-auto">
-        <Form
-          onSubmit={(e) => {
-            e.preventDefault();
-            filterCharacters(searchInput);
-          }}
-        >
-          <h1 className="fs-3">Character Search</h1>
-          <Form.Group className="mb-3">
-            <Form.Label htmlFor="search">Search Text</Form.Label>
-            <Form.Control
-              id="search"
-              placeholder="Name or title"
-              value={searchInput}
-              onChange={(e) => setSearchInput(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group>
-            <Form.Control
-              className="btn btn-primary w-50"
-              type="submit"
-              value="Submit"
-            />
-          </Form.Group>
-        </Form>
+        <h1 className="fs-3">Character Search</h1>
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="search">Name or Title</Form.Label>
+          <Form.Control
+            id="search"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </Form.Group>
       </Card>
       <Card className="my-3 px-3 py-2 text-center mx-auto">
         {matchingCharacters.length}
         {matchingCharacters.length === 1 ? ' result' : ' results'}
       </Card>
-      <Container className="d-flex flex-row flex-wrap gap-3 justify-content-center">
+      <div className="d-flex flex-row flex-wrap gap-3 justify-content-center">
         {matchingCharacters.map((c) => (
           <Card className="character-card" key={c.id}>
             <Card.Img
@@ -69,7 +50,7 @@ function Search({ characters }) {
             </Card.Body>
           </Card>
         ))}
-      </Container>
+      </div>
     </>
   );
 }
